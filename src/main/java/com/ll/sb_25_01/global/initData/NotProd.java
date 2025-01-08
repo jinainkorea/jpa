072ffc50eb41cfba9395d4c7e8ceaaac799d2630
jinainkorea <kimjina1152@gmail.com>
2025-01-08 11:40:46 +0900

    step2, article initData

--- src/main/java/com/ll/sb_25_01/global/initData/NotProd.java
+++ src/main/java/com/ll/sb_25_01/global/initData/NotProd.java
@@ -14,8 +14,14 @@ public class NotProd {
     @Bean
     public ApplicationRunner initNotProdData(MemberService memberService, ArticleService articleService) {
         return args -> {
-            RsData<Member> joinRs = memberService.join("user1", "1234");
-            System.out.println(joinRs);
+            Member member1 = memberService.join("user1", "1234").getData();
+            Member member2 = memberService.join("user2", "1234").getData();
+
+            articleService.write(member1.getId(), "제목1", "내용1");
+            articleService.write(member1.getId(), "제목2", "내용2");
+
+            articleService.write(member2.getId(), "제목3", "내용3");
+            articleService.write(member2.getId(), "제목4", "내용4");
         };
     }
 }
